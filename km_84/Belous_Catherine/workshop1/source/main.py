e8530ede18461a77b7aaa9707017fefb9cd5fb90
katebeloussmile <katyabelousal@gmail.com>
2018-11-08 12:31:14 +0200

    workshop1

---
+++ km_84/Belous_Catherine/workshop1/source/main.py
@@ -0,0 +1,14 @@
+"""
+Твоє завдання
+"""
+import km_84.Belous_Catherine.workshop1.source.lib.moodle as mymodule
+
+from  km_84.Belous_Catherine.workshop1.source.lib.moodle import per
+
+a = float(input("Катет 1: "))
+b = float(input("Катет 2: "))
+c = ((a**2 + b**2))**0.5
+
+per()
+mymodule.square()
+
